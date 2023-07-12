07dea1046527893a8962fd724a07837d51e1ab7c
Anna Ilieva <ilieva.anna.a@gmail.com>
2023-07-12 22:08:43 +0200

    Added NewPost file and event listener

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import classes from "./PostsList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 const PostsList = () => {
   return (
-    <ul className={classes.posts}>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
         <Post author="Maximilian" body="React.js is awesome!" />
         <Post author="Manuel" body="Check out the full course!" />
-    </ul>
-  )
+      </ul>
+    </>
+  );
 };
 
 export default PostsList;

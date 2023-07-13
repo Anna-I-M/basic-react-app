50ec6dbf22707d846d601ba4b24088a67ca484c3
Anna Ilieva <ilieva.anna.a@gmail.com>
2023-07-13 22:12:35 +0200

    Added Modal, MainHeader files and logic

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,34 @@
 import classes from "./PostsList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import { useState } from "react";
+import Modal from "./Modal";
 
-const PostsList = () => {
+const PostsList = ({isPosting, onStopPosting}) => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
-        <Post author="Manuel" body="Check out the full course!" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author={enteredAuthor} body={enteredBody} />
       </ul>
     </>
   );
